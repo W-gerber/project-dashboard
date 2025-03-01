26eb68a6aed8f031f6fbc7fb3946e030600f53f1
W-Gerber <wgerber04@icloud.com>
2025-03-01 20:41:17 +0200

    Initialize Spring Boot project with basic structure and configurations

---
+++ backend/dashboard/src/main/java/com/wgerb/dashboard/project_dashboard/HelloController.java
@@ -0,0 +1,12 @@
+package com.wgerb.dashboard.project_dashboard;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World from Spring Boot!";
+    }
+}
---
+++ backend/dashboard/src/test/java/com/wgerb/dashboard/project_dashboard/ProjectDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wgerb.dashboard.project_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
